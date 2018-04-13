7d12be3f6dbafd48a1ab91e39652366d84bfeb11
flywen <flywen@flywen.cn>
2018-04-13 14:55:21 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import network
+import time
+from machine import Pin
+from simple import MQTTClient
+import dht
 
 SSID = "Otec_wuhan"
 PASSWD = "Otec8888"
@@ -11,6 +15,32 @@ def connect_net():
     if net.isconnected():
         print("network is ok...")
     else:
+        print("Ready...")
         net.connect(SSID, PASSWD)
+        print("network is %s" % SSID)
 
 connect_net()
+
+def do_dht():
+    ht = dht.DHT11(Pin(2))
+    ht.measure()
+    ht_h = str(ht.humidity())
+    ht_t = str(ht.temperature())
+    return ht_h, ht_t
+
+#h, t = do_dht() 
+#print("humidity is %s,temprature is %s" % (h, t))
+
+def send_mq():
+    h, t = do_dht()
+    print("h=%s,t=%s"%(h,t))
+    m = MQTTClient('nodemcu', '192.168.1.112')
+    m.connect()
+    m.publish('hh', h)
+    time.sleep(0.2)
+    m.publish('tt', t)
+    m.disconnect()
+
+while True:
+    time.sleep(5)
+    send_mq()
